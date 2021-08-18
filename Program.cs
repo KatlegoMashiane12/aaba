da8e462ff6958d513378c0dd5964c686657863b4
KatlegoMashiane <Katlego.timothy.9@gmail.com>
2021-08-18 10:34:24 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Class_Vaccine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Enter ID digits");
+            Console.ReadLine();
+
+            Console.WriteLine("Enter the surname");
+            Console.ReadLine();
+
+            Console.WriteLine("Enter your Firstname");
+            Console.ReadLine();
+
+            Console.WriteLine("Enter your Gender (Either M(Male) or F(Female)");
+            Console.ReadLine();
+
+            Console.WriteLine("Enter your Date Of Birth");
+            Console.ReadLine();
+
+            Console.WriteLine("Enter your Province");
+            Console.ReadLine();
+
+            Console.WriteLine("Hello User, Please insert the required information");
+        }
+    }
+}
